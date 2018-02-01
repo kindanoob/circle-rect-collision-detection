d84c5a2a4eef581510c3c77e7073dc0a51280352
kindanoob <evgpavlov1@gmail.com>
2018-02-01 18:10:23 +0500

    renamed constants, replaced const with constexpr, added comments to functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,124 @@
 #include <iostream>
 #include <cmath>
-
+#include <string>
+#include <utility>
 #include <SFML/Graphics.hpp>
 
-const double DX = 0.1;
-const double DY = 0.1;
-const double kCircleRadius = 40; //in pixels
+constexpr int kWindowWidth = 800;
+constexpr int kWindowHeight = 600;
+const std::string kWindowTitle = "Collision detection";
+const sf::Color kBackgroundColor = sf::Color::White;
+
+constexpr double kCircleRadius = 40; //in pixels
+const sf::Color kCircleDefaultColor = sf::Color::Magenta;
+const sf::Color kCircleCollisionColor = sf::Color::Red;
+constexpr double kCircleInitialPositionX = 100.0;
+constexpr double kCircleInitialPositionY = 100.0;
+
+constexpr int kRectangleWidth = 200;
+constexpr int kRectangleHeight = 100;
+const sf::Color kRectangleDefaultColor = sf::Color::Blue;
+constexpr double kRectangleInitialPositionX = 400.0;
+constexpr double kRectangleInitialPositionY = 300.0;
+
+constexpr double kDx = 0.1;
+constexpr double kDy = 0.1;
+
+constexpr double kEpsilon = 1e-3;
+constexpr double kTimeAdjustmentCoeff = 400.0;
 
 
 
 
-double dist_squared(const sf::Vector2f& u, const sf::Vector2f& v) {
+double DistSquared(const sf::Vector2f& u, const sf::Vector2f& v) {
     double dx = u.x - v.x;
     double dy = u.y - v.y;
     return dx * dx + dy * dy;
 }
 
-double dist(const sf::Vector2f& u, const sf::Vector2f& v) {
-    return sqrt(dist_squared(u, v));
+double Dist(const sf::Vector2f& u, const sf::Vector2f& v) {
+    return sqrt(DistSquared(u, v));
 }
 
-double len(const sf::Vector2f& v) {
-    return dist(v, sf::Vector2f(0, 0));
+double Len(const sf::Vector2f& v) {
+    return Dist(v, sf::Vector2f(0, 0));
 }
 
-void normalize(sf::Vector2f& v) {
-    double len_v = len(v);
+void Normalize(sf::Vector2f& v) {
+    double len_v = Len(v);
+    if (len_v < kEpsilon) {
+        return;
+    }
     v.x /= len_v;
     v.y /= len_v;
 }
 
-sf::Vector2f quadratic_equation(double a, double b, double c) {
-    double sqrt_d = sqrt(b * b - 4 * a * c);
-    double x1 = (-b - sqrt_d) / (2 * a);
-    double x2 = (-b + sqrt_d) / (2 * a);
-    return sf::Vector2f(x1, x2);
-}
-
-//a, b - coefficients of the direction vector of line
-double circle_line_intersection(sf::CircleShape& circle, double dx, double dy, sf::Vector2f& corner) {
-    double diff_y = circle.getPosition().y - corner.y;
-    double diff_x = circle.getPosition().x - corner.x;
-    double a = diff_y / diff_x;
-    double b = circle.getPosition().y - a * circle.getPosition().x;
-    ///Now we know the line equation y = a * x + b
-    sf::Vector2f intersection_points = quadratic_equation(a * a + 1, 2 * (a * b - a * circle.getPosition().y - circle.getPosition().x), circle.getPosition().x * circle.getPosition().x + (b - circle.getPosition().y) * (b - circle.getPosition().y) - circle.getRadius() * circle.getRadius());
-    sf::Vector2f first_point = sf::Vector2f(intersection_points.x, a * intersection_points.x + b);
-    sf::Vector2f second_point = sf::Vector2f(intersection_points.y, a * intersection_points.y + b);
-    double dist1 = dist(first_point, corner);
-    double dist2 = dist(second_point, corner);
-    double shift = std::min(dist1 ,dist2);
-    return shift;
-}
-
-bool check_collision(sf::RectangleShape& rect, sf::CircleShape& circle) {
-    sf::Vector2f center = circle.getPosition() - rect.getPosition();
+// This function returns true if there is a collision between circle and rectangle
+// and false otherwise.
+bool CheckCollision(sf::RectangleShape& rect, sf::CircleShape& circle) {
+    // Calculate distance from circle's center to rectangle's center
+    sf::Vector2f center_vector = circle.getPosition() - rect.getPosition();
     double rect_w = rect.getSize().x;
     double rect_h = rect.getSize().y;
     double r = circle.getRadius();
-    double dist_x = fabs(center.x) - rect_w / 2;
-    double dist_y = fabs(center.y) - rect_h / 2;
-    if ((dist_x > r) || (dist_y > r)) {
+    double Dist_x = fabs(center_vector.x) - rect_w / 2;
+    double Dist_y = fabs(center_vector.y) - rect_h / 2;
+    // First perform two simple tests for collision
+    // If distance between centers is too large, there cannot be a collision.
+    if ((Dist_x > r) || (Dist_y > r)) {
         return false;
     }
-    if ((dist_x <= 0) || (dist_y <= 0)) {
+    // If distance is too small, there is definitely a collision.
+    if ((Dist_x < kEpsilon) || (Dist_y < kEpsilon)) {
         return true;
     }
-    bool close_to_corner = (fabs(center.x) - rect_w / 2) * (fabs(center.x) - rect_w / 2) + (fabs(center.y) - rect_h / 2) * (fabs(center.y) - rect_h / 2) <= r * r;
+    // If the above two tests were inconclusive, do one more test for collision.
+    // Compare distance from rectangle's corner closest to the circle to circle's center
+    double dx = fabs(center_vector.x) - rect_w / 2;
+    double dy = fabs(center_vector.y) - rect_h / 2;
+    bool close_to_corner =  dx * dx + dy * dy < (r * r + kEpsilon);
     if (!close_to_corner) {
         return false;
     }
     return true;
 }
 
-sf::Vector2f resolve_collision(sf::RectangleShape& rect, sf::CircleShape& circle, double dx, double dy) {
+
+std::pair<bool, sf::Vector2f> QuadraticEquation(double a, double b, double c) {
+    double d = b * b - 4 * a * c;
+    if (d < 0) {
+        std::cerr << "Discriminant is negative." << std::endl;
+        return std::make_pair(false, sf::Vector2f());
+    }
+    double sqrt_d = sqrt(d);
+    double x1 = (-b - sqrt_d) / (2 * a);
+    double x2 = (-b + sqrt_d) / (2 * a);
+    return std::make_pair(true, sf::Vector2f(x1, x2));
+}
+
+
+double CircleLineIntersection(sf::CircleShape& circle, double dx, double dy, sf::Vector2f& corner) {
+    double diff_y = circle.getPosition().y - corner.y;
+    double diff_x = circle.getPosition().x - corner.x;
+    //a, b are the coefficients of the direction vector of line
+    double a = diff_y / diff_x;
+    double b = circle.getPosition().y - a * circle.getPosition().x;
+    ///Now we know the line equation y = a * x + b
+    auto intersection_points = QuadraticEquation(a * a + 1, 2 * (a * b - a * circle.getPosition().y - circle.getPosition().x), circle.getPosition().x * circle.getPosition().x + (b - circle.getPosition().y) * (b - circle.getPosition().y) - circle.getRadius() * circle.getRadius());
+    // If there is no intersection, return zero
+    if (!intersection_points.first) {
+        return 0.0;
+    }
+    sf::Vector2f first_point = sf::Vector2f(intersection_points.second.x, a * intersection_points.second.x + b);
+    sf::Vector2f second_point = sf::Vector2f(intersection_points.second.y, a * intersection_points.second.y + b);
+    double Dist1 = Dist(first_point, corner);
+    double Dist2 = Dist(second_point, corner);
+    double shift = std::min(Dist1 ,Dist2);
+    return shift;
+}
+
+
+sf::Vector2f ResolveCollision(sf::RectangleShape& rect, sf::CircleShape& circle, double dx, double dy) {
     sf::Vector2f center = circle.getPosition() - rect.getPosition();
     double rect_w = rect.getSize().x;
     double rect_h = rect.getSize().y;
@@ -105,41 +150,42 @@ sf::Vector2f resolve_collision(sf::RectangleShape& rect, sf::CircleShape& circle
         }
     }
 
-    double shift = circle_line_intersection(circle, dx, dy, corner);
+    double shift = CircleLineIntersection(circle, dx, dy, corner);
     sf::Vector2f diff = circle.getPosition() - corner;
-    normalize(diff);
+    Normalize(diff);
     diff.x *= shift;
     diff.y *= shift;
     sf::Vector2f new_pos = circle.getPosition() + diff;
     return new_pos;
 }
 
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Collision detection");
+    sf::RenderWindow window(sf::VideoMode(kWindowWidth, kWindowHeight), kWindowTitle);
     window.setVerticalSyncEnabled(true);
 
     sf::RectangleShape rect;
-    rect.setFillColor(sf::Color::Blue);
-    rect.setSize(sf::Vector2f(200, 100));
-    rect.setOrigin(sf::Vector2f(100, 50));
-    rect.setPosition(sf::Vector2f(400, 300));
+    rect.setFillColor(kRectangleDefaultColor);
+    rect.setSize(sf::Vector2f(kRectangleWidth, kRectangleHeight));
+    rect.setOrigin(sf::Vector2f(kRectangleWidth / 2, kRectangleHeight / 2));
+    rect.setPosition(sf::Vector2f(kRectangleInitialPositionX, kRectangleInitialPositionY));
 
     sf::CircleShape circle;
     circle.setRadius(kCircleRadius);
     circle.setOrigin(sf::Vector2f(kCircleRadius, kCircleRadius));
-    circle.setPosition(sf::Vector2f(100, 100));
-    circle.setFillColor(sf::Color::Magenta);
+    circle.setPosition(sf::Vector2f(kCircleInitialPositionX, kCircleInitialPositionY));
+    circle.setFillColor(kCircleDefaultColor);
 
     sf::Clock clock;
     sf::Vector2f mouse_window_prev;
 
-    double dx = 0;
-    double dy = 0;
+    double dx = 0.0;
+    double dy = 0.0;
 
     while (window.isOpen()) {
         double dt = clock.getElapsedTime().asMicroseconds();
         clock.restart();
-        dt /= 400;
+        dt /= kTimeAdjustmentCoeff;
 
         sf::Event e;
         while (window.pollEvent(e)) {
@@ -147,31 +193,27 @@ int main() {
                 window.close();
             }
         }
-        sf::Vector2f v = sf::Vector2f(circle.getPosition());
-        double x = v.x;
-        double y = v.y;
+        
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            dx = -DX;
+            dx = -kDx;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            dx = DX;
+            dx = kDx;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            dy = -DY;
+            dy = -kDy;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            dy = DY;
+            dy = kDy;
         }
-        x += dx * dt;
-        y += dy * dt;
-        circle.setPosition(x, y);
+        sf::Vector2f circle_position = sf::Vector2f(circle.getPosition());        
+        circle.setPosition(circle_position + sf::Vector2f(dx * dt, dy * dt));
 
         sf::Vector2i mouse = sf::Mouse::getPosition(window);
         sf::Vector2f mouse_window_curr = sf::Vector2f(static_cast<double>(mouse.x), static_cast<double>(mouse.y));
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            sf::Vector2f dist = mouse_window_curr - circle.getPosition();
-            double r = circle.getRadius();
-            if (len(dist) <= r) {
+            sf::Vector2f Dist = mouse_window_curr - circle.getPosition();
+            if (Len(Dist) <= circle.getRadius()) {
                 sf::Vector2f mouse_diff = mouse_window_curr - mouse_window_prev;
                 circle.setPosition(circle.getPosition() + mouse_diff);
                 dx = mouse_diff.x;
@@ -179,17 +221,17 @@ int main() {
             }
         }
 
-        if (check_collision(rect, circle)) {
-            sf::Vector2f new_pos = resolve_collision(rect, circle, dx, dy);
-            circle.setFillColor(sf::Color::Red);
+        if (CheckCollision(rect, circle)) {
+            sf::Vector2f new_pos = ResolveCollision(rect, circle, dx, dy);
+            circle.setFillColor(kCircleCollisionColor);
             circle.setPosition(new_pos);
         } else {
-            circle.setFillColor(sf::Color::Magenta);
+            circle.setFillColor(kCircleDefaultColor);
         }
-        dx = 0;
-        dy = 0;
+        dx = 0.0;
+        dy = 0.0;
 
-        window.clear(sf::Color::White);
+        window.clear(kBackgroundColor);
         window.draw(rect);
         window.draw(circle);
         window.display();
